5516e671ebdceb37c56b04d709a514e47ba42a87
remy <remy@localhost.localdomain>
2017-05-09 19:02:13 -0400

    updating files2

--- DB.py
+++ DB.py
@@ -1,9 +1,23 @@
 
 
-import json
+from tinydb import TinyDB, Query
 
 
-class db: 
+class DB: 
+
     
+    def __init__(self, dataFile)
+    # file that will hold the .json database
+    global db = TinyDB(dataFile)
+
+
+    def insert(jsonData)
+        db
+
+    def remove(key)
+
+
+
+
 
 
